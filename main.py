b70cecc581544d6ac68153d344261a5bdf1fa2d3
kangying <kangyinglee0501@outlook.com>
2023-09-24 15:31:44 +0900

    add ff256output

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ def print_hi(name):
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     print_hi('PyCharm')
-    print("main changed")
+    print("this is ff-256 branch")
 
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
